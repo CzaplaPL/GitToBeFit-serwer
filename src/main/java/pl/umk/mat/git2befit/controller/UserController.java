50240b2aea2c71f6e68c8e64759b19286250cda2
KacperCzajkowski <kaczaper931@gmail.com>
2020-12-17 12:31:29 +0100

    Dodanie endpointa pozwalającego na update usera

--- src/main/java/pl/umk/mat/git2befit/controller/UserController.java
+++ src/main/java/pl/umk/mat/git2befit/controller/UserController.java
@@ -1,6 +1,7 @@
 package pl.umk.mat.git2befit.controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import pl.umk.mat.git2befit.model.User;
 import pl.umk.mat.git2befit.repository.UserRepository;
@@ -25,9 +26,23 @@ public class UserController {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
+
     @GetMapping("/{id}")
     public Optional<User> getUser(@PathVariable long id){
         return userRepository.findById(id);
     }
 
+    @PutMapping("/{id}")
+    public void update(@PathVariable long id,
+                        @RequestBody User user){
+        Optional<User> userFromTheDB = userRepository.findById(id);
+        if (userFromTheDB.isEmpty()) {
+            //TODO Tu do rzucenia wyjątek ale nie wiem jaki (Kacper)
+        } else {
+            User tempUser = userFromTheDB.get();
+            tempUser.setEmail(user.getEmail());
+            tempUser.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(tempUser);
+        }
+    }
 }
