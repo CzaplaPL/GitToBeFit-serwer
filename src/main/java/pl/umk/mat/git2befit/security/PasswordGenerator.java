fca716a620405281ddf0dcd6ffc29376d0ea2692
KacperCzajkowski <kaczaper931@gmail.com>
2021-01-29 22:18:11 +0100

    Naprawienie generowanie nowych haseł.

--- src/main/java/pl/umk/mat/git2befit/security/PasswordGenerator.java
+++ src/main/java/pl/umk/mat/git2befit/security/PasswordGenerator.java
@@ -3,11 +3,11 @@ package pl.umk.mat.git2befit.security;
 import com.github.curiousoddman.rgxgen.RgxGen;
 
 
-import static pl.umk.mat.git2befit.validation.UserValidationService.PASSWORD_REGEX;
+import static pl.umk.mat.git2befit.validation.UserValidationService.PASSWORD;
 
 public class PasswordGenerator {
     public static String generateRandomPassword(){
-        RgxGen rgxGen = new RgxGen(PASSWORD_REGEX);
+        RgxGen rgxGen = new RgxGen(PASSWORD.pattern());
         return rgxGen.generate().replaceAll(" ", "");
     }
 
