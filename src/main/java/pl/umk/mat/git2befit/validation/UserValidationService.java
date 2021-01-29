4cafe204c61f3ef8ef596cd64a9bb636ad82fa76
mpuch12 <45654602+mpuch12@users.noreply.github.com>
2021-01-29 10:55:42 +0100

    Zwiększenie wydajności klasy oraz zmienienie nie działających wyrażeń z poprzedniego commita

--- src/main/java/pl/umk/mat/git2befit/validation/UserValidationService.java
+++ src/main/java/pl/umk/mat/git2befit/validation/UserValidationService.java
@@ -6,22 +6,24 @@ import pl.umk.mat.git2befit.exceptions.EmailValidationException;
 import pl.umk.mat.git2befit.exceptions.WeakPasswordException;
 import pl.umk.mat.git2befit.model.entity.User;
 
+import java.util.regex.Pattern;
+
 @Service
 public class UserValidationService {
 
-    private final static String emailRegex = "^[\\w!#$%&'+/=?`{|}~^-]+(?:\\.[\\w!#$%&'+/=?`{|}~^-]+)@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
-    public final static String PASSWORD_REGEX = "(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.*[@!#$%^&+=])(?=\\S+$).{8,}";
+    private final static Pattern EMAIL = Pattern.compile("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$");
+    public final static Pattern PASSWORD = Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#$%^&+=])(?=\\S+$).{8,}");
     //8 znaków, mała i duża litera, cyfra, znak specjalny
 
     private UserValidationService() {}
 
     public static void validateEmail(String email) throws EmailValidationException {
-        if(!email.matches(emailRegex))
+        if(!EMAIL.matcher(email).matches())
             throw new EmailValidationException("bad email");
     }
 
     public static void validatePassword(String password) throws WeakPasswordException {
-        if(!password.matches(PASSWORD_REGEX))
+        if(!PASSWORD.matcher(password).matches())
             throw new WeakPasswordException("weak password");
     }
 
