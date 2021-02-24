3cd1c912d77d9cffe309a71afed1614a8048ac67
mpuch12 <45654602+mpuch12@users.noreply.github.com>
2021-02-24 22:04:20 +0100

    Dodanie akcesorów dostępu do obiektu ScheduleType

--- src/main/java/pl/umk/mat/git2befit/model/entity/workout/conditions/ScheduleType.java
+++ src/main/java/pl/umk/mat/git2befit/model/entity/workout/conditions/ScheduleType.java
@@ -14,6 +14,22 @@ public class ScheduleType implements Serializable {
     @Column(nullable = false, unique = true)
     private String name;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public ScheduleType(){}
 
     public ScheduleType(long id, String name) {
