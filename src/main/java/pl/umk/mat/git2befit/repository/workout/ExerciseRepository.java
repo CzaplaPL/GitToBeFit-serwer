7c2c141e9614b0745b00bba3f1a85c0afa2b194d
KacperCzajkowski <kaczaper931@gmail.com>
2021-03-08 13:48:57 +0100

    Naniesienie poprawek w CardioTrainingPlan. Usunięcie todo w ExerciseRepository.

--- src/main/java/pl/umk/mat/git2befit/repository/workout/ExerciseRepository.java
+++ src/main/java/pl/umk/mat/git2befit/repository/workout/ExerciseRepository.java
@@ -9,33 +9,32 @@ import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     List<Exercise> getAllByTrainingTypes_Name(String exerciseForm_name);
-    // todo zrobic, zeby szukalo wzgledem typu treningu
     @Query(value = "SELECT\n" +
-                    "       *\n" +
-                    "FROM\n" +
-                    "     exercises e\n" +
-                    "WHERE e.id in (SELECT\n" +
-                    "                     ex_eq.exercise_id\n" +
-                    "              from\n" +
-                    "                   exercise_equipment ex_eq\n" +
-                    "              WHERE\n" +
-                    "                    ex_eq.equipment_id IN (SELECT " +
-                    "                                               ID " +
-                    "                                           FROM" +
-                    "                                               equipment equip" +
-                    "                                           WHERE " +
-                    "                                               equip.name = 'Bez sprzetu'))" +
-                    "       AND e.id in (SELECT\n" +
-                    "                       ttoe.exercise_id\n" +
-                    "                    FROM\n" +
-                    "                       training_types_of_exercises ttoe\n" +
-                    "                    WHERE\n" +
-                    "                       ttoe.training_type_id IN (SELECT " +
-                    "                                                      tt.id " +
-                    "                                                 FROM " +
-                    "                                                       training_types tt" +
-                    "                                                 WHERE " +
-                    "                                                       name = ?1))",
+            "       *\n" +
+            "FROM\n" +
+            "     exercises e\n" +
+            "WHERE e.id in (SELECT\n" +
+            "                     ex_eq.exercise_id\n" +
+            "              from\n" +
+            "                   exercise_equipment ex_eq\n" +
+            "              WHERE\n" +
+            "                    ex_eq.equipment_id IN (SELECT " +
+            "                                               ID " +
+            "                                           FROM" +
+            "                                               equipment equip" +
+            "                                           WHERE " +
+            "                                               equip.name = 'Bez sprzetu'))" +
+            "       AND e.id in (SELECT\n" +
+            "                       ttoe.exercise_id\n" +
+            "                    FROM\n" +
+            "                       training_types_of_exercises ttoe\n" +
+            "                    WHERE\n" +
+            "                       ttoe.training_type_id IN (SELECT " +
+            "                                                      tt.id " +
+            "                                                 FROM " +
+            "                                                       training_types tt" +
+            "                                                 WHERE " +
+            "                                                       name = ?1))",
             nativeQuery = true)
     List<Exercise> getAllWithNoEquipmentForTrainingTypeName(String name);
 }
--- src/main/java/pl/umk/mat/git2befit/service/workout/factory/implementation/CardioTrainingPlan.java
+++ src/main/java/pl/umk/mat/git2befit/service/workout/factory/implementation/CardioTrainingPlan.java
@@ -17,6 +17,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class CardioTrainingPlan implements TrainingPlanInterface {
     private final String TRAINING_TYPE = "CARDIO";
     private final static int SINGLE_STEP = 3;
+    private final static int MAX_RAND_TRIES = 1000;
     private final ExerciseRepository exerciseRepository;
 
     public CardioTrainingPlan(ExerciseRepository exerciseRepository) {
@@ -39,24 +40,47 @@ public class CardioTrainingPlan implements TrainingPlanInterface {
             rolledExercises.addAll(filteredListOfExercises);
         } else {
             int counter = 0;
-            while (rolledExercises.size() < exercisesToGet &&
-                    counter < filteredListOfExercises.size()) {
+            // losowanie ćwiczeń tak, żeby się nie powtarzały
+            while (rolledExercises.size() < exercisesToGet && counter < MAX_RAND_TRIES) {
+                // losowanie liczby z przedzialu od 0 do filtered.size
                 int actualRandom = randomIndexGen.nextInt(filteredListOfExercises.size());
+                // sciaganie cwiczenia
                 Exercise exercise = filteredListOfExercises.get(actualRandom);
+                // test, czy na dana partie ciala nie ma za duzo cwiczen
                 if (checkIfBodyPartIsNotOverloaded(rolledExercises, exercise)) {
+                    // jesli nie jest to dodajemy cwiczenie
                     rolledExercises.add(exercise);
+                    // usuwamy z listy, zeby nie moglo byc wiecej pobrane
+                    filteredListOfExercises.remove(actualRandom);
                 }
+                // licznik obrotow petli
                 counter++;
             }
-            for (int i = 0; ((i < filteredListOfExercises.size()) && (rolledExercises.size() != exercisesToGet)); i++) {
-                Exercise actualExercise = filteredListOfExercises.get(i);
-                if (!rolledExercises.contains(actualExercise)) {
-                    rolledExercises.add(actualExercise);
-                }
+            // wypelnienie listy wylosowanych cwiczen do konca
+            while (!filteredListOfExercises.isEmpty() && rolledExercises.size() != exercisesToGet) {
+                // losowanie liczby z przedzialu od 0 do filtered.size
+                int actualRandom = randomIndexGen.nextInt(filteredListOfExercises.size());
+                // sciaganie cwiczenia
+                Exercise exercise = filteredListOfExercises.get(actualRandom);
+                // dodanie cwiczenia
+                rolledExercises.add(exercise);
+                // usuniecie cwiczenia z listy pozostalych
+                filteredListOfExercises.remove(exercise);
             }
         }
-        if (rolledExercises.size() < exercisesToGet) {
 
+        if (rolledExercises.size() < exercisesToGet) {
+            filteredListOfExercises = exerciseRepository.getAllWithNoEquipmentForTrainingTypeName(TRAINING_TYPE);
+            while (!filteredListOfExercises.isEmpty() && rolledExercises.size() != exercisesToGet) {
+                // losowanie liczby z przedzialu od 0 do filtered.size
+                int actualRandom = randomIndexGen.nextInt(filteredListOfExercises.size());
+                // sciaganie cwiczenia
+                Exercise exercise = filteredListOfExercises.get(actualRandom);
+                // dodanie cwiczenia
+                rolledExercises.add(exercise);
+                // usuniecie cwiczenia z listy pozostalych
+                filteredListOfExercises.remove(exercise);
+            }
         }
         List<ExerciseExecution> exerciseExecutions = getExercisesExecutions(rolledExercises);
         training.setExercisesExecutions(exerciseExecutions);
