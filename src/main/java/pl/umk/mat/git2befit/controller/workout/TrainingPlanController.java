f4c10f529645530ec6c2255ce7ad432cf2fc0dde
KacperCzajkowski <kaczaper931@gmail.com>
2021-04-05 20:28:51 +0200

    Rozpoczęcie implementacji usuwania

--- src/main/java/pl/umk/mat/git2befit/controller/workout/TrainingPlanController.java
+++ src/main/java/pl/umk/mat/git2befit/controller/workout/TrainingPlanController.java
@@ -127,4 +127,10 @@ public class TrainingPlanController {
             return ResponseEntity.notFound().header("Cause", exception.getMessage()).build();
         }
     }
+
+    @DeleteMapping("/{trainingPlanId}")
+    public ResponseEntity<?> deleteTrainingPlan(
+            @PathVariable Long trainingPlanId,
+            @RequestHeader(value = "Authorization") String authorizationToken
+    ) {}
 }
