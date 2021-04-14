c698b0d105876c19b5219e156e118cc8f99fa545
mpuch12 <45654602+mpuch12@users.noreply.github.com>
2021-04-14 20:06:37 +0200

    Dodanie endpointu na pobieranie sumy kontrolnej

--- src/main/java/pl/umk/mat/git2befit/controller/workout/SynchronizeController.java
+++ src/main/java/pl/umk/mat/git2befit/controller/workout/SynchronizeController.java
@@ -2,8 +2,8 @@ package pl.umk.mat.git2befit.controller.workout;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.umk.mat.git2befit.service.SynchService;
@@ -13,15 +13,17 @@ import pl.umk.mat.git2befit.service.SynchService;
 public class SynchronizeController {
 
     private final SynchService synchService;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public SynchronizeController(SynchService synchService) {
+    public SynchronizeController(SynchService synchService, JdbcTemplate jdbcTemplate) {
         this.synchService = synchService;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @GetMapping
-    public ResponseEntity<?> checkSum(@RequestHeader String authorization){
-        var checkSum = synchService.getCheckSum();
-        return ResponseEntity.ok(checkSum);
+    public ResponseEntity<?> checkSum(){
+        var checkSums = synchService.getCheckSum();
+        return ResponseEntity.ok(checkSums);
     }
 }
